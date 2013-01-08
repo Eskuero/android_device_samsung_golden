a5eab9cc0af2a33ebf1a0041372c8c0cb2422e25
Oliver Garcia Albertos <oliverarafo@gmail.com>
2013-01-08 22:13:53 +0000

    config: Update partitions and add recovery config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,16 +3,20 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/golden/include
 # Platform 
 TARGET_BOARD_PLATFORM := montblanc
 TARGET_BOOTLOADER_BOARD_NAME := golden
+TARGET_NO_BOOTLOADER := true
 
 # Architecture
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH=arm
+TARGET_ARCH := arm
 
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x4B000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x12DE00000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x1000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1000000
+BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p19
 
@@ -28,7 +32,7 @@ BOARD_FORCE_RAMDISK_ADDRESS := 0x02000000
 # Graphics
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/samsung/golden/prebuilt/system/lib/egl/egl.cfg
-COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE -DSTE_HDMI
+COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -43,7 +47,9 @@ BOARD_UMS_LUNFILE := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/f
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun0/file"
 
 # Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_INITRC = device/samsung/golden/recovery/init.golden.rc
+BOARD_CUSTOM_GRAPHICS := device/samsung/golden/recovery/graphics.c
 
-
-# Build opensource specific platform libs
+# Build opensource specific board libs
 TARGET_BUILD_OPENSOURCE_LIBS := true
