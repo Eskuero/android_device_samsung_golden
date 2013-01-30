aa5bea6f43556f2cc3addccb733a4c2e45c26b10
Oliver Garcia Albertos <oliverarafo@gmail.com>
2013-01-30 19:52:01 +0000

    board: bcm4334_B2 is our revision

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ USE_BLUETOOTH_SAP := true
 
 # Wifi
 BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_WLAN_DEVICE_REV            := bcm4330_b1
+BOARD_WLAN_DEVICE_REV            := bcm4334_B2
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
