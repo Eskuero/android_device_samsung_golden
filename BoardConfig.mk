b86044ca5e7d3ec28dab4db230f7fee1cef036a9
OliverG96 <oliverarafo@gmail.com>
2013-07-04 23:44:48 +0100

    init: Use stock android init.rc
    I cannot see any big change between both except the btld thing for FMRadio, which is not working and personally I will never try to make it to work.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,7 +113,6 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 
 # Recovery
-TARGET_PROVIDES_INIT_RC := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_INITRC = device/samsung/golden/recovery/recovery.rc
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/golden/recovery/graphics.c
