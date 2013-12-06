70676b2548b6c3bbfa19edd9240703c711582967
OliverG96 <oliverarafo@gmail.com>
2013-12-06 18:40:43 +0000

    rootdir: Update wifi supplicants for 4.4

--- golden.mk
+++ golden.mk
@@ -89,7 +89,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    $(LOCAL_PATH)/configs/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    $(LOCAL_PATH)/configs/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 PRODUCT_PACKAGES += \
     libnetcmdiface
 PRODUCT_PROPERTY_OVERRIDES += \
